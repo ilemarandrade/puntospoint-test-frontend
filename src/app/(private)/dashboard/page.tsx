231fb4a113c2f3f4aa1c389ec04e5cd21275cc3a
Ilemar Andrade <ilemarandrade@gmail.com>
2025-03-09 21:52:08 -0400

    feat: :sparkles: add get movements to dashboard view

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,16 +1,28 @@
+'use client';
+
 import AsideDashboard from '@/components/aside-dashboard';
 import FiltersDashboard from '@/components/filters-dashboard';
 import MainRechart from '@/components/recharts/main-recharts';
-import { data } from '@/components/recharts/main-recharts/data-mock';
-import { EnumFiltersTags } from '@/types/filters';
+import useGetMovements from '@/hooks/use-get-movements';
+import { IFiltersDasboard } from '@/types/recharts';
 import { Grid2 } from '@mui/material';
+import { useCallback, useState } from 'react';
 
 const DashboardPage = () => {
+  const [filters, setFilters] = useState<IFiltersDasboard>();
+
+  const { data } = useGetMovements({ filters });
+
+  const onChangeFilter = useCallback((filter: IFiltersDasboard) => {
+    setFilters(filter);
+  }, []);
+
   return (
     <Grid2 container component="main" className="py-8 px-16" spacing={3}>
       <Grid2 component="section" size={{ md: 9 }} className="space-y-8">
-        <FiltersDashboard />
-        <MainRechart data={data} tagsSelected={[EnumFiltersTags.CLIENTS]} />
+        <FiltersDashboard onChangeFilter={onChangeFilter} />
+
+        <MainRechart data={data} tagsSelected={filters?.tags} />
       </Grid2>
       <Grid2 component="aside" size={{ md: 3 }}>
         <AsideDashboard />
