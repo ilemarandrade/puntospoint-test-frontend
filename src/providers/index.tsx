50c303e218706d098608c458fcfc58e27565d2dd
Ilemar Andrade <ilemarandrade@gmail.com>
2025-03-09 20:52:23 -0400

    feat: :sparkles: add provider of react query

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -2,14 +2,21 @@
 
 import theme from '@/root/theme';
 import { ThemeProvider } from '@mui/material';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactNode } from 'react';
 
 interface IProps {
   children: ReactNode;
 }
 
+const queryClient = new QueryClient();
+
 const Providers: React.FC<IProps> = ({ children }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </QueryClientProvider>
+  );
 };
 
 export default Providers;
