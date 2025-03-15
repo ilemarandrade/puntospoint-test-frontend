383a939e9cf7a90a1f7d58f801e3ebd190a2e01d
Ilemar Andrade <ilemarandrade@gmail.com>
2025-03-15 11:08:36 -0400

    refactor: :fire: remove tag innecesary

--- src/components/top-bar/mobile-top-bar.tsx
+++ src/components/top-bar/mobile-top-bar.tsx
@@ -52,114 +52,109 @@ const MobileTopBar = ({
         <MenuIcon />
       </IconButton>
 
-      <nav>
-        <Drawer
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block' },
-            '& .MuiDrawer-paper': {
-              boxSizing: 'border-box',
-              width: drawerWidth,
-            },
-          }}
-        >
-          <Box
-            sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-          >
-            <Box sx={{ p: 2, textAlign: 'center' }}>
-              <Image
-                src="/puntospoint-logo.svg"
-                alt="Logo"
-                width={120}
-                height={120}
-                className="mx-auto mb-2"
-              />
-            </Box>
-            <Divider />
-            <List sx={{ flexGrow: 1 }}>
-              {linksMenu.map((item) => (
-                <ListItem key={item.title} disablePadding>
-                  <ListItemButton
-                    component={Link}
-                    href={item.url}
-                    selected={pathname === item.url}
-                    sx={{
-                      '&.Mui-selected': {
-                        backgroundColor: 'primary.main',
+      <Drawer
+        variant="temporary"
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
+        }}
+        sx={{
+          display: { xs: 'block' },
+          '& .MuiDrawer-paper': {
+            boxSizing: 'border-box',
+            width: drawerWidth,
+          },
+        }}
+      >
+        <Box sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+          <Box sx={{ p: 2, textAlign: 'center' }}>
+            <Image
+              src="/puntospoint-logo.svg"
+              alt="Logo"
+              width={120}
+              height={120}
+              className="mx-auto mb-2"
+            />
+          </Box>
+          <Divider />
+          <List sx={{ flexGrow: 1 }}>
+            {linksMenu.map((item) => (
+              <ListItem key={item.title} disablePadding>
+                <ListItemButton
+                  component={Link}
+                  href={item.url}
+                  selected={pathname === item.url}
+                  sx={{
+                    '&.Mui-selected': {
+                      backgroundColor: 'primary.main',
+                      color: 'primary.contrastText',
+                      '&:hover': {
+                        backgroundColor: 'primary.dark',
+                      },
+                      '& .MuiListItemIcon-root': {
                         color: 'primary.contrastText',
-                        '&:hover': {
-                          backgroundColor: 'primary.dark',
-                        },
-                        '& .MuiListItemIcon-root': {
-                          color: 'primary.contrastText',
-                        },
+                      },
+                    },
+                  }}
+                >
+                  <ListItemIcon
+                    sx={{
+                      '& svg': {
+                        color: pathname === item.url ? 'white' : 'text.primary',
                       },
                     }}
                   >
-                    <ListItemIcon
-                      sx={{
-                        '& svg': {
-                          color:
-                            pathname === item.url ? 'white' : 'text.primary',
-                        },
-                      }}
-                    >
-                      {item.title === 'Dashboard' && <DashboardRoundedIcon />}
-                      {item.title === 'Clientes' && <PeopleRoundedIcon />}
-                      {item.title === 'Reglas de acumulación' && (
-                        <BalanceRoundedIcon />
-                      )}
-                    </ListItemIcon>
-                    <ListItemText
-                      primary={item.title}
-                      sx={{
-                        fontSize: '0.875rem',
-                        fontWeight: pathname === item.url ? 600 : 400,
-                        color: pathname === item.url ? 'white' : 'text.primary',
-                      }}
-                    />
-                  </ListItemButton>
-                </ListItem>
-              ))}
-            </List>
-            <Divider />
-            <List>
-              <ListItem disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    <PersonRoundedIcon />
+                    {item.title === 'Dashboard' && <DashboardRoundedIcon />}
+                    {item.title === 'Clientes' && <PeopleRoundedIcon />}
+                    {item.title === 'Reglas de acumulación' && (
+                      <BalanceRoundedIcon />
+                    )}
                   </ListItemIcon>
                   <ListItemText
-                    primary="Editar Perfil"
+                    primary={item.title}
                     sx={{
                       fontSize: '0.875rem',
+                      fontWeight: pathname === item.url ? 600 : 400,
+                      color: pathname === item.url ? 'white' : 'text.primary',
                     }}
                   />
                 </ListItemButton>
               </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton sx={{ color: 'error.main' }}>
-                  <ListItemIcon sx={{ color: 'inherit' }}>
-                    <LogoutRoundedIcon />
-                  </ListItemIcon>
-                  <ListItemText
-                    primary="Cerrar Sesión"
-                    sx={{
-                      fontSize: '0.875rem',
-                      color: 'inherit',
-                    }}
-                  />
-                </ListItemButton>
-              </ListItem>
-            </List>
-          </Box>
-        </Drawer>
-      </nav>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <PersonRoundedIcon />
+                </ListItemIcon>
+                <ListItemText
+                  primary="Editar Perfil"
+                  sx={{
+                    fontSize: '0.875rem',
+                  }}
+                />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton sx={{ color: 'error.main' }}>
+                <ListItemIcon sx={{ color: 'inherit' }}>
+                  <LogoutRoundedIcon />
+                </ListItemIcon>
+                <ListItemText
+                  primary="Cerrar Sesión"
+                  sx={{
+                    fontSize: '0.875rem',
+                    color: 'inherit',
+                  }}
+                />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Box>
+      </Drawer>
     </>
   );
 };
