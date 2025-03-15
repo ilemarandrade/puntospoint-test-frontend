1415ba7e1d78534b417a87f1e73aeb002266161e
Ilemar Andrade <ilemarandrade@gmail.com>
2025-03-15 13:05:39 -0400

    feat: :sparkles: initialize GA

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,9 +1,12 @@
 'use client';
 
+import { env } from '@/root/env';
 import theme from '@/root/theme';
 import { ThemeProvider } from '@mui/material';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactNode } from 'react';
+import { usePathname } from 'next/navigation';
+import { ReactNode, useEffect } from 'react';
+import ReactGA from 'react-ga4';
 
 interface IProps {
   children: ReactNode;
@@ -12,6 +15,10 @@ interface IProps {
 const queryClient = new QueryClient();
 
 const Providers: React.FC<IProps> = ({ children }) => {
+  useEffect(() => {
+    ReactGA.initialize(env.GA_MEASUREMENT_ID);
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>{children}</ThemeProvider>
